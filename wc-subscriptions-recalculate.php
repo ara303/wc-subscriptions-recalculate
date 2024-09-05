f5d5d5ada846b57650c2e585d8ae21972f0acb5a
E <769754+ara303@users.noreply.github.com>
2024-09-05 20:16:22 +0100

    add --dry-run flag option

--- wc-subscriptions-recalculate.php
+++ wc-subscriptions-recalculate.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WC Subscriptions Recalculate
  * Version: 1.0.1
- * Description: Bulk update existing WooCommerce Subscriptions when the prices of products change, via a WP-CLI command (`wcsr recalculate`).
+ * Description: Bulk update existing WooCommerce Subscriptions when the prices of products change, via a WP-CLI command (`wp wcsr recalculate`).
  * Author: ara303 
  * Author URI: http://github.com/ara303
  * Requires at least: 6.0
@@ -14,7 +14,9 @@ if ( ! defined( 'WP_CLI' ) ) {
 }
 
 class WC_Subscriptions_Recalculate {
-    public function recalculate() {
+    public function recalculate( $args, $assoc_args ) {
+        $dry_run = isset( $assoc_args['dry-run'] ) ?: false;
+        
         if( ! class_exists( 'WC_Subscriptions' ) ){
             WP_CLI::error( "WooCommerce Subscriptions is not active." );
         }
@@ -58,20 +60,27 @@ class WC_Subscriptions_Recalculate {
                     $new_price = $new_price_excl_tax;
                 }
 
-                $item->set_subtotal( $new_price );
-                $item->set_total( $new_price );
-                $item->save();
+                if( ! $dry_run ){
+                    $item->set_subtotal( $new_price );
+                    $item->set_total( $new_price );
+                    $item->save();
 
-                WP_CLI::log( "Updated price for subscription ID {$subscription_post->ID} to {$new_price} (was {$old_price})." );
+                    WP_CLI::log( "Price for subscription ID {$subscription_post->ID} updated to {$new_price} (was {$old_price})." );
+                } else {
+                    WP_CLI::log( "Dry run mode. Price for subscription ID {$subscription_post->ID} WAS NOT updated to {$new_price} (was {$old_price})." );
+                }
+                    
             }
 
-            $subscription->calculate_totals();
-            $subscription->save();
-
-            WP_CLI::success( "Subscription ID {$subscription_post->ID} updated." );
+            if( ! $dry_run ){
+                $subscription->calculate_totals();
+                $subscription->save();
+    
+                WP_CLI::success( "Subscription ID {$subscription_post->ID} updated." );
+            }
         }
 
-        WP_CLI::success( "All subscriptions recalculated." );
+        WP_CLI::success( "WooCommmerce Subscriptions Recalculate ran successfully!" );
     }
 }
 
