e4d4ced647a94e9fe77f92af656d43380f7ea70f
E <769754+ara303@users.noreply.github.com>
2024-09-05 19:45:53 +0100

    factor in vat/tax

--- wc-subscriptions-recalculate.php
+++ wc-subscriptions-recalculate.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: WC Subscriptions Recalculate
- * Version: 1.0.0
- * Description: Bulk update existing WooCommerce Subscriptions when the prices of products change, via a WP-CLI command (`wcsr recalculate`). Verbose by default.
+ * Version: 1.0.1
+ * Description: Bulk update existing WooCommerce Subscriptions when the prices of products change, via a WP-CLI command (`wcsr recalculate`).
  * Author: ara303 
  * Author URI: http://github.com/ara303
  * Requires at least: 6.0
@@ -26,7 +26,7 @@ class WC_Subscriptions_Recalculate {
         ]);
 
         if( ! $subscriptions ){
-            WP_CLI::success( "No active subscriptions found. Nothing has been recalculated." );
+            WP_CLI::success( "No active subscriptions found." );
             return;
         }
 
@@ -43,27 +43,35 @@ class WC_Subscriptions_Recalculate {
                 $product = wc_get_product( $product_id );
 
                 if( ! $product ){
-                    WP_CLI::warning( "No product ID {$product_id} found for subscription ID {$subscription_post->ID}. That product could have been deleted." );
+                    WP_CLI::warning( "Product ID {$product_id} not found within subscription ID {$subscription_post->ID}." );
                     continue;
                 }
 
                 $old_price = $item->get_total();
-                $new_price = $product->get_price();
+
+                $new_price_incl_tax = wc_get_price_including_tax( $product );
+                $new_price_excl_tax = wc_get_price_excluding_tax( $product );
+                $tax_option = get_option( 'woocommerce_tax_display_shop' );
+                if( $tax_option === "incl" ){
+                    $new_price = $new_price_incl_tax;
+                } else {
+                    $new_price = $new_price_excl_tax;
+                }
 
                 $item->set_subtotal( $new_price );
                 $item->set_total( $new_price );
                 $item->save();
 
-                WP_CLI::log( "Set new price for subscription ID: {$subscription_post->ID}, previously: {$old_price} and now: {$new_price}." );
+                WP_CLI::log( "Updated price for subscription ID {$subscription_post->ID} to {$new_price} (was {$old_price})." );
             }
 
             $subscription->calculate_totals();
             $subscription->save();
 
-            WP_CLI::success( "Subscription ID {$subscription_post->ID} recalculated." );
+            WP_CLI::success( "Subscription ID {$subscription_post->ID} updated." );
         }
 
-        WP_CLI::success( "All active subscriptions recalculated." );
+        WP_CLI::success( "All subscriptions recalculated." );
     }
 }
 
